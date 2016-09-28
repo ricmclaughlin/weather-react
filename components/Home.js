094a90fd27fe4b26286906e145f4113fc3cec778
ric mclaughlin <ric@mclaughlin.today>
2016-09-28 08:40:53 -0700

    initial project setup

---
+++ components/Home.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>My New Fancy App</h1>
+        <p className="lead">Some Fancy Motto</p>
+        <Link to="/#">
+          <button className="btn btn-lg btn-success" type="button">Get Started</button>
+        </Link>
+      </div>
+    )
+  }
+});
+
+module.exports = Home;
