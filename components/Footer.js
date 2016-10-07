8dcdccbf0687827aa974131f1a3d4366cefb475a
ric mclaughlin <ric@mclaughlin.today>
2016-10-06 19:40:03 -0700

    clean

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React, { PropTypes } from 'react';
-
-export default function Footer (props) {
-  return (
-    <p id="footer">
-      Made with <span className="stylish">style</span> by <a href="http://ric.mclaughlin.today">Ric McLaughlin</a>
-    </p>
-  )
-}  
